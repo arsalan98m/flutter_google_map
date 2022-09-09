619480efa634df754f1aabed81f8ba3a9d1551b8
arsalan manzoor <arsalan1998m@gmail.com>
2022-09-09 18:46:56 +0500

    google map marker custom info window

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 
+import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -14,10 +15,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final Completer<GoogleMapController> _controller = Completer();
-  Uint8List? markerImage;
+  final CustomInfoWindowController _customInfoWindowController =
+      CustomInfoWindowController();
 
-  List images = ['assets/car.png', 'assets/car.png', 'assets/car.png'];
+  final Completer<GoogleMapController> _controller = Completer();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(24.9141617, 67.082216),
@@ -26,7 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List<Marker> markers = [];
 
-  final List<LatLng> latLng = [
+  final List<LatLng> latLng = <LatLng>[
     LatLng(24.9141617, 67.082216),
     LatLng(24.9368, 67.0760),
     LatLng(24.932152, 67.086014)
@@ -38,32 +39,80 @@ class _HomeScreenState extends State<HomeScreen> {
     loadData();
   }
 
-  // *** Generating Car Icon *** //
-  Future<Uint8List> getBytesFromAssets(String path, int width) async {
-    ByteData data = await rootBundle.load(path);
-    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
-        targetWidth: width);
-
-    ui.FrameInfo fi = await codec.getNextFrame();
-
-    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
-        .buffer
-        .asUint8List();
-  }
-
   loadData() async {
-    for (int i = 0; i <= latLng.length; i++) {
-      final Uint8List markerIcon = await getBytesFromAssets(images[i], 50);
-
+    for (int i = 0; i < latLng.length; i++) {
       markers.add(
         Marker(
-          markerId: MarkerId(
-            i.toString(),
-          ),
-          position: latLng[i],
-          infoWindow: InfoWindow(title: 'This is title marker $i'),
-          icon: BitmapDescriptor.fromBytes(markerIcon),
-        ),
+            markerId: MarkerId(
+              i.toString(),
+            ),
+            position: latLng[i],
+            icon: BitmapDescriptor.defaultMarker,
+            onTap: () {
+              _customInfoWindowController.addInfoWindow!(
+                Container(
+                  width: 300,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 300,
+                        height: 100,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                'https://cdn.vox-cdn.com/thumbor/VF4Bp99v90IMMcFhQDnJqxI_AJs=/0x0:5996x4003/1200x900/filters:focal(1003x1633:1961x2591)/cdn.vox-cdn.com/uploads/chorus_image/image/59732085/2021_03_23_Merois_008.12.jpg',
+                              ),
+                              fit: BoxFit.fitWidth,
+                              filterQuality: FilterQuality.high),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                          color: Colors.red,
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(top: 10, left: 10, right: 10),
+                        child: Row(
+                          children: const [
+                            SizedBox(
+                              width: 100,
+                              child: Text(
+                                'Beef Tacos',
+                                maxLines: 1,
+                                overflow: TextOverflow.fade,
+                                softWrap: false,
+                              ),
+                            ),
+                            Spacer(),
+                            Text(
+                              '.3 mi.',
+                              // widget.data!.date!,
+                            )
+                          ],
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+                        child: Text(
+                          'Help me finish these tacos! I got a platter from Costco and it’s too much.',
+                          maxLines: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                latLng[i],
+              );
+            }),
       );
 
       setState(() {});
@@ -74,13 +123,30 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: GoogleMap(
-          onMapCreated: ((GoogleMapController controller) {
-            _controller.complete(controller);
-          }),
-          initialCameraPosition: _kGooglePlex,
-          mapType: MapType.normal,
-          markers: Set<Marker>.of(markers),
+        child: Stack(
+          children: [
+            GoogleMap(
+              onMapCreated: ((GoogleMapController controller) {
+                _controller.complete(controller);
+                _customInfoWindowController.googleMapController = controller;
+              }),
+              initialCameraPosition: _kGooglePlex,
+              mapType: MapType.normal,
+              markers: Set<Marker>.of(markers),
+              onTap: (position) {
+                _customInfoWindowController.hideInfoWindow!();
+              },
+              onCameraMove: (position) {
+                _customInfoWindowController.onCameraMove!();
+              },
+            ),
+            CustomInfoWindow(
+              controller: _customInfoWindowController,
+              height: 200,
+              width: 300,
+              offset: 35,
+            )
+          ],
         ),
       ),
     );
