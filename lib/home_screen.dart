78451b6ede1201a5d6d8b2298a0b6cf750d09c0a
arsalan manzoor <arsalan1998m@gmail.com>
2022-09-09 14:31:10 +0500

    added multiple markers on map

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -18,6 +18,42 @@ class _HomeScreenState extends State<HomeScreen> {
     zoom: 14.4746,
   );
 
+  List<Marker> _markers = [];
+
+  final List<Marker> _list = const [
+    Marker(
+      markerId: MarkerId('1'),
+      position: LatLng(24.9141617, 67.082216),
+      infoWindow: InfoWindow(title: 'My Position'),
+    ),
+    Marker(
+      markerId: MarkerId('2'),
+      position: LatLng(24.9368, 67.0760),
+      infoWindow: InfoWindow(title: 'Water Pump'),
+    ),
+    Marker(
+      markerId: MarkerId('3'),
+      position: LatLng(24.9324, 67.0873),
+      infoWindow: InfoWindow(title: 'Lucky one mall'),
+    ),
+    Marker(
+      markerId: MarkerId('4'),
+      position: LatLng(24.9449517288, 67.0855423611),
+      infoWindow: InfoWindow(title: 'Sohrab Goth Bus Stop'),
+    ),
+    Marker(
+      markerId: MarkerId('5'),
+      position: LatLng(33.9391, 67.7100),
+      infoWindow: InfoWindow(title: 'Afghanistan'),
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _markers.addAll(_list);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +64,7 @@ class _HomeScreenState extends State<HomeScreen> {
           }),
           initialCameraPosition: _kGooglePlex,
           mapType: MapType.normal,
+          markers: Set<Marker>.of(_markers),
         ),
       ),
     );
