56962c4cbaa4ee36d85dc49967e1cc8421f1aafd
arsalan manzoor <arsalan1998m@gmail.com>
2022-09-09 19:44:48 +0500

    added test file

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
-import 'dart:typed_data';
-import 'dart:ui' as ui;
+import 'dart:collection';
 
-import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// Polygon ka faida kya hai, Polygon ko jo use hai woh yeh hai k for example ap kisi ek specific area k andar operate kar rahy hain for example hum ny jo red area draw kia hai gulshan sy lucky one or water pump tak ka usmy operate kar rahy hain so jo bhi apka user is area sy bahar hai ap usko login na karny dain uski location agar issy match nhi karti tu login na karny dain these are the applications of plygon
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -15,9 +14,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final CustomInfoWindowController _customInfoWindowController =
-      CustomInfoWindowController();
-
   final Completer<GoogleMapController> _controller = Completer();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
@@ -25,98 +21,27 @@ class _HomeScreenState extends State<HomeScreen> {
     zoom: 14.4746,
   );
 
-  List<Marker> markers = [];
+  final Set<Marker> markers = {};
+  Set<Polygon> polygon = HashSet<Polygon>();
 
-  final List<LatLng> latLng = <LatLng>[
+  List<LatLng> points = [
     LatLng(24.9141617, 67.082216),
+    LatLng(24.932152, 67.086014),
     LatLng(24.9368, 67.0760),
-    LatLng(24.932152, 67.086014)
+    LatLng(24.9141617, 67.082216),
   ];
 
   @override
   void initState() {
     super.initState();
-    loadData();
-  }
 
-  loadData() async {
-    for (int i = 0; i < latLng.length; i++) {
-      markers.add(
-        Marker(
-            markerId: MarkerId(
-              i.toString(),
-            ),
-            position: latLng[i],
-            icon: BitmapDescriptor.defaultMarker,
-            onTap: () {
-              _customInfoWindowController.addInfoWindow!(
-                Container(
-                  width: 300,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 300,
-                        height: 100,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                'https://cdn.vox-cdn.com/thumbor/VF4Bp99v90IMMcFhQDnJqxI_AJs=/0x0:5996x4003/1200x900/filters:focal(1003x1633:1961x2591)/cdn.vox-cdn.com/uploads/chorus_image/image/59732085/2021_03_23_Merois_008.12.jpg',
-                              ),
-                              fit: BoxFit.fitWidth,
-                              filterQuality: FilterQuality.high),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10.0),
-                          ),
-                          color: Colors.red,
-                        ),
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(top: 10, left: 10, right: 10),
-                        child: Row(
-                          children: const [
-                            SizedBox(
-                              width: 100,
-                              child: Text(
-                                'Beef Tacos',
-                                maxLines: 1,
-                                overflow: TextOverflow.fade,
-                                softWrap: false,
-                              ),
-                            ),
-                            Spacer(),
-                            Text(
-                              '.3 mi.',
-                              // widget.data!.date!,
-                            )
-                          ],
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-                        child: Text(
-                          'Help me finish these tacos! I got a platter from Costco and it’s too much.',
-                          maxLines: 2,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                latLng[i],
-              );
-            }),
-      );
-
-      setState(() {});
-    }
+    polygon.add(Polygon(
+      polygonId: PolygonId('1'),
+      points: points,
+      fillColor: Colors.red.withOpacity(0.3),
+      geodesic: true,
+      strokeWidth: 4,
+    ));
   }
 
   @override
@@ -128,24 +53,11 @@ class _HomeScreenState extends State<HomeScreen> {
             GoogleMap(
               onMapCreated: ((GoogleMapController controller) {
                 _controller.complete(controller);
-                _customInfoWindowController.googleMapController = controller;
               }),
               initialCameraPosition: _kGooglePlex,
               mapType: MapType.normal,
-              markers: Set<Marker>.of(markers),
-              onTap: (position) {
-                _customInfoWindowController.hideInfoWindow!();
-              },
-              onCameraMove: (position) {
-                _customInfoWindowController.onCameraMove!();
-              },
+              polygons: polygon,
             ),
-            CustomInfoWindow(
-              controller: _customInfoWindowController,
-              height: 200,
-              width: 300,
-              offset: 35,
-            )
           ],
         ),
       ),
